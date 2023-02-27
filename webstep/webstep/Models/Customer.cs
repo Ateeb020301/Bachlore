bafc6143bb3b694686db9adcb35f500a4c7b14a9
Ateeb020301 <ateeb@live.no>
2023-02-27 12:39:36 +0100

    Ateeb Branch

---
+++ webstep/webstep/Models/Customer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Linq;
+using HotChocolate;
+using IdentityModel.Client;
+using IdentityServer4.Extensions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using NodaTime.Calendars;
+
+namespace webstep.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    using NodaTime;
+
+    using webstep.GraphQL;
+
+    public class Customer : BaseModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public override int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Adresse { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Tlf { get; set; }
+
+        public ICollection<Project> Projects { get; set; } = new List<Project>();
+
+    }
+}
