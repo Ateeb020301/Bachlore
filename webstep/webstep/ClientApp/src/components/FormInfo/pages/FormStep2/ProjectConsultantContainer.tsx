4a999338958d4303f4ab9e1e02826889822b37b2
Ateeb020301 <ateeb@live.no>
2023-04-12 19:26:14 +0200

    ok

--- webstep/webstep/ClientApp/src/components/FormInfo/pages/FormStep2/ProjectConsultantContainer.tsx
+++ webstep/webstep/ClientApp/src/components/FormInfo/pages/FormStep2/ProjectConsultantContainer.tsx
@@ -2,21 +2,16 @@ import { useMutation, useQuery } from '@apollo/client';
 import React from 'react';
 import { DELETE_PROJECTCONSULTANT, GET_PROJECTCONSULTANTS } from '../../../../api/contract/queries';
 import { GetProjectConsultantPayload2 } from '../../../../api/contract/payloads';
-
 import { Loading } from '../../../../page/Utils/Loading';
 import { useForm } from '../../context/FormContext';
 import { toast } from 'react-toastify';
 import { ProjectConsultantDisplay } from './ProjectConsultantDisplay';
 
-//GQL pagination skip const
-const skipAmount = 0;
-//GQL pagination take const
-const takeAmount = 50;
 //sette projectconsultant id == id til nåcærende prosjekt
 export const ProjectConsultantContainer: React.FC = () => {
-    const { loading, error, data, refetch } = useQuery<GetProjectConsultantPayload2>(GET_PROJECTCONSULTANTS)
+    const { loading, error, data } = useQuery<GetProjectConsultantPayload2>(GET_PROJECTCONSULTANTS)
     
-    const {state, dispatch} = useForm()
+    const {state} = useForm()
     let containerContent;
     const [deletePC] = useMutation<number, { input: { id: number } }>(DELETE_PROJECTCONSULTANT,{
         refetchQueries: [
@@ -27,31 +22,31 @@ export const ProjectConsultantContainer: React.FC = () => {
         awaitRefetchQueries: true,
     });
 
-    const sendDeleteRequest = (id:number)=>{
-        console.log(id);
-        let deleteId=0;
-        data?.projectConsultant.map((aPC)=>{
-            aPC.consultant.map((aConsultant)=>{
-                if(aConsultant.id==id && state.projectId==aPC.project.id){
-                    deleteId=aPC.id;
+    // const sendDeleteRequest = (id:number)=>{
+    //     console.log(id);
+    //     let deleteId=0;
+    //     data?.projectConsultant.map((aPC)=>{
+    //         aPC.consultant.map((aConsultant)=>{
+    //             if(aConsultant.id==id && state.projectId==aPC.project.id){
+    //                 deleteId=aPC.id;
 
-                }
-            })
-        })
+    //             }
+    //         })
+    //     })
         
-        deletePC({ variables: { input: {id: deleteId} } })
-        .then((res) => {
-            toast.success('Consultant fra Project slettet', {
-                position: toast.POSITION.BOTTOM_RIGHT
-            })
-        })
-        .catch((e) => {
-            toast.error('Noe gikk galt ved sletting av Consultant fra Projects', {
-                position: toast.POSITION.BOTTOM_RIGHT
-            })
-            console.log(e);
-        });
-    }
+    //     deletePC({ variables: { input: {id: deleteId} } })
+    //     .then((res) => {
+    //         toast.success('Consultant fra Project slettet', {
+    //             position: toast.POSITION.BOTTOM_RIGHT
+    //         })
+    //     })
+    //     .catch((e) => {
+    //         toast.error('Noe gikk galt ved sletting av Consultant fra Projects', {
+    //             position: toast.POSITION.BOTTOM_RIGHT
+    //         })
+    //         console.log(e);
+    //     });
+    // }
     if (data) {
         containerContent = 
             data.projectConsultant.map((aPC)=>{
