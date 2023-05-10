a5eeb031ec818d6289647edcb4f52fb8a3badbc6
moha0102 <65622942+moha0102@users.noreply.github.com>
2023-05-10 18:16:45 +0200

    test

--- webstep/webstep/ClientApp/src/page/contracts/contracts.tsx
+++ webstep/webstep/ClientApp/src/page/contracts/contracts.tsx
@@ -1,37 +1,38 @@
-import React from 'react';
-import './contracts.css';
-import { FullPageContent } from '../Utils/FullPageContent';
-import { ContractCalendarContainer } from './contracts/ContractCalendarContainer';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { Box } from '@mui/material';
-import { ContractsContainer } from './contractscontainer';
+import React from "react";
+import "./contracts.css";
+import { FullPageContent } from "../Utils/FullPageContent";
+import { ContractCalendarContainer } from "./contracts/ContractCalendarContainer";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Box } from "@mui/material";
+import { ContractsContainer } from "./contractscontainer";
 
 export const Contracts: React.FC = () => {
-    return (
-        <Box sx={{}}>
-            <Box sx={{height: '100%', m: 1, display: 'flex', flexDirection: 'column'}}>
-                <Box
-                    sx={{
-                    boxShadow: "0px 0px 3px 0px rgba(0,0,0,0.1)",
-                    display: "flex",
-                    flexBasis: "100%",
-                    flexWrap: "wrap",
-                    background: "#ffffff",
-                    borderRadius: "5px",
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                    }}
-                >
-                    <ContractsContainer />
-                </Box>
-
-                <Box sx={{ width: '1440px', py: 1}}>
-                    <ContractCalendarContainer/>
-                </Box>
-                <ToastContainer />
-            </Box>
-            
+  return (
+    <Box sx={{}}>
+      <Box
+        sx={{ height: "100%", m: 1, display: "flex", flexDirection: "column" }}
+      >
+        <Box
+          sx={{
+            boxShadow: "0px 0px 3px 0px rgba(0,0,0,0.1)",
+            display: "flex",
+            width: "85vw",
+            flexWrap: "wrap",
+            background: "#ffffff",
+            borderRadius: "5px",
+            justifyContent: "space-between",
+            alignSelf: "center",
+          }}
+        >
+          <ContractsContainer />
         </Box>
-    );
+
+        <Box sx={{ maxWidth: "85vw", alignSelf: "center", py: 1 }}>
+          <ContractCalendarContainer />
+        </Box>
+        <ToastContainer />
+      </Box>
+    </Box>
+  );
 };
